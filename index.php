4b7ef576289e597e1aae20d583a05903807ccdaf
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2024-10-20 20:49:44 -0400

    More placeholder files

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Photo ABCD</title>
+		<link rel="stylesheet" type="text/css" href="styles.css"/>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+	</head>
+	<body>
+	
+	</body>
+</html>
